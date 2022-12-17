6dcb71de232b04037271ca1b46e906cb727a621b
Iliya5digital <115736491+Iliya5digital@users.noreply.github.com>
2022-12-17 22:56:16 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let score = 0
+document.getElementById('totalscore').innerHTML = score;
+
+function addscore1 () {
+    score += 1;
+    document.getElementById('totalscore').innerHTML = score;
+}
+
+function addscore2 () {
+    score += 10;
+    document.getElementById('totalscore').innerHTML = score;
+}
+
+function addscore3 () {
+    score += 50;
+    document.getElementById('totalscore').innerHTML = score;
+}
+
+function addscore4 () {
+    score += 100;
+    document.getElementById('totalscore').innerHTML = score;
+}
